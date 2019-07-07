187fe4225d902cd484c33c18a17fa0312548c1a3
Jhoan Angeles Vega <jhoanvga@gmail.com>
2019-07-07 15:16:07 -0500

    colores

--- lib/alta_cliente.dart
+++ lib/alta_cliente.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-
 import 'main.dart';
 
 class ClientPage extends StatefulWidget {
@@ -10,6 +9,15 @@ class ClientPage extends StatefulWidget {
 class _AltaCliente extends State<ClientPage> {
   final GlobalKey<FormState> _keyFormClient = new GlobalKey<FormState>();
   final GlobalKey<ScaffoldState> _keyFormScal = new GlobalKey<ScaffoldState>();
+  List<String> _colonias = new List<String>();
+  String coloniaSelected;
+  bool activeAddress;
+
+  @override
+  void initState() {
+    _colonias.addAll(["Seleccione una colonia", "Villas de santiago", "Sauces", "Lomas de Casa blanca"]);
+    coloniaSelected = "Seleccione una colonia";
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -21,55 +29,121 @@ class _AltaCliente extends State<ClientPage> {
       body: SafeArea(
         top: false,
         bottom: false,
-          child: new Form(
-              key: _keyFormClient,
-              autovalidate: false,
-              child: new ListView(
-            padding: const EdgeInsets.symmetric(horizontal: 16.0),
-            children: <Widget>[
-              new TextFormField(
-                decoration: const InputDecoration(
-                  icon: const Icon(Icons.assignment_ind),
-                  hintText: 'Ingresa RFC.',
-                  labelText: 'RFC',
+        child: new Form(
+            key: _keyFormClient,
+            autovalidate: false,
+            child: new ListView(
+              padding: const EdgeInsets.symmetric(horizontal: 16.0),
+              children: <Widget>[
+                 TextFormField(
+                  decoration: const InputDecoration(
+                    icon: const Icon(Icons.assignment_ind),
+                    hintText: 'Ingresa RFC.',
+                    labelText: 'RFC',
+                  ),
                 ),
-              ),
-              new TextFormField(
-                decoration: const InputDecoration(
-                  icon: const Icon(Icons.person),
-                  hintText: 'Ingresa nombre completo.',
-                  labelText: 'Nombre',
+                 TextFormField(
+                  decoration: const InputDecoration(
+                    icon: const Icon(Icons.person),
+                    hintText: 'Ingresa razon social.',
+                    labelText: 'Razon social',
+                  ),
                 ),
-              ),
-              new TextFormField(
-                decoration: const InputDecoration(
-                  icon: const Icon(Icons.email),
-                  hintText: 'Ingrese un correo electronico.',
-                  labelText: 'Correo electronico',
+                 TextFormField(
+                  decoration: const InputDecoration(
+                    icon: const Icon(Icons.email),
+                    hintText: 'Ingrese un correo electronico.',
+                    labelText: 'Correo electronico',
+                  ),
+                  validator: (value) =>
+                      isValidEmail(value) ? null : 'Ingrese un correo valido.',
+                  keyboardType: TextInputType.emailAddress,
+                ), RaisedButton(onPressed: null, child: new Text("Presione para agregar una direccion")),
+                activeAddress ? new Container() :
+                 TextFormField(
+                  decoration: const InputDecoration(
+                    icon: const Icon(Icons.home),
+                    hintText: 'Ingrese un codigo postal',
+                    labelText: 'Codigo postal',
+                  ),
+                  validator: (value) =>
+                      isValidEmail(value) ? null : 'Ingrese un codigo postal.',
+                  keyboardType: TextInputType.emailAddress,
+                ), activeAddress ? new Container() :
+                 DropdownButtonFormField(
+                   validator: (value){
+                     if(value == "Seleccione una colonia"){
+                       return "Seleccione una colonia del listado";
+                     }
+                   },
+                   decoration: InputDecoration(icon: Icon(Icons.home)),
+                  value: coloniaSelected,
+                   hint: new Text("Seleccione una colonia"),
+                   items: _colonias
+                    .map((label) => DropdownMenuItem(
+                      child: Text(label),
+                      value: label,
+                      )).toList(),
+                     onChanged: (value) {
+                       setState(() {
+                         coloniaSelected = value;
+                       });
+                     }
+                ),activeAddress ? new Container() :
+                 TextFormField(
+                  decoration: const InputDecoration(
+                    icon: const Icon(Icons.home),
+                    hintText: 'Ingrese la calle',
+                    labelText: 'Calle',
+                  ),
+                  validator: (value) =>
+                      isValidEmail(value) ? null : 'Ingrese una calle.',
+                  keyboardType: TextInputType.emailAddress,
+                ),activeAddress ? new Container() :
+                 TextFormField(
+                  decoration: const InputDecoration(
+                    icon: const Icon(Icons.home),
+                    hintText: 'Ingrese el número exterior',
+                    labelText: '# exterior',
+                  ),
+                  validator: (value) => isValidEmail(value)
+                      ? null
+                      : 'Ingrese un numero exterior.',
+                  keyboardType: TextInputType.emailAddress,
+                ),activeAddress ? new Container() :
+                 TextFormField(
+                  decoration: const InputDecoration(
+                    icon: const Icon(Icons.home),
+                    hintText: 'Número el numero interior',
+                    labelText: '# interior',
+                  ),
+                  validator: (value) => isValidEmail(value)
+                      ? null
+                      : 'Ingrese un numero interior.',
+                  keyboardType: TextInputType.emailAddress,
                 ),
-                validator: (value) => isValidEmail(value) ? null : 'Ingrese un correo valido.',
-                keyboardType: TextInputType.emailAddress,
-              ),
-              Padding(
-                padding: const EdgeInsets.all(18.0),
-                child: new RaisedButton(
-                  elevation: 8.0,
-                    shape: BeveledRectangleBorder(
-                      borderRadius: BorderRadius.all(Radius.circular(3.0)),
-                    ),
-                    splashColor: Colors.black26,
-                    child: const Text('Guardar'),
-                    color: Theme.of(context).accentColor,
-                    onPressed: (){_neverSatisfied();}),
-
-              ),
-            ],
-          )),
+                Padding(
+                  padding: const EdgeInsets.all(18.0),
+                  child: new RaisedButton(
+                      elevation: 8.0,
+                      shape: BeveledRectangleBorder(
+                        borderRadius: BorderRadius.all(Radius.circular(3.0)),
+                      ),
+                      splashColor: Colors.black26,
+                      child: const Text('Guardar'),
+                      color: Theme.of(context).accentColor,
+                      onPressed: () {
+                       if (_keyFormClient.currentState.validate())
+                        _neverSatisfied();
+                      }),
+                ),
+              ],
+            )),
       ),
     );
   }
 
-  Future<void> _neverSatisfied()  {
+  Future<void> _neverSatisfied() {
     return showDialog<void>(
       context: context,
       barrierDismissible: false, // user must tap button!
@@ -78,8 +152,7 @@ class _AltaCliente extends State<ClientPage> {
           title: Text('Alta de cliente'),
           content: SingleChildScrollView(
             child: ListBody(
-              children: <Widget>[
-                Text('¿Esta seguro de crear el cliente?.')],
+              children: <Widget>[Text('¿Esta seguro de crear el cliente?')],
             ),
           ),
           actions: <Widget>[
@@ -106,7 +179,14 @@ class _AltaCliente extends State<ClientPage> {
   }
 
   bool isValidEmail(String input) {
-    final RegExp regex = new RegExp(r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
+    final RegExp regex = new RegExp(
+        r"^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?(?:\.[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,253}[a-zA-Z0-9])?)*$");
     return regex.hasMatch(input);
+  }
+
+  void _onChanged(String value) {
+    setState(() {
+      coloniaSelected = value;
+    });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,6 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
 
 
-
   @override
   Widget build(BuildContext context) {
     TextEditingController editingController = TextEditingController();
@@ -95,22 +94,64 @@ class _ClienteList extends ListTile {
           title: Text(cliente.nombre),
           subtitle: Text(cliente.correo),
           leading: CircleAvatar(
-            child: Text(cliente.nombre[0]),
+            backgroundColor: getColor(cliente.nombre[0]),
+            child: Text(cliente.nombre[0], style: TextStyle(color: Colors.white) ,),
           ),
           trailing: Icon(Icons.keyboard_arrow_right),
         onTap: (){
 
         }
       );
+
+  static Color getColor(String letraInicial) {
+    var colores_letra_inicial = {
+      "A" : Colors.pink, "B" : Colors.pink[800],
+      "C":Colors.red, "D" : Colors.red[800],
+      "E" : Colors.orange, "F" : Colors.orange[800],
+      "G" : Colors.amber, "H" : Colors.yellow,
+      "I" : Colors.lime, "J" : Colors.lime[800] ,
+      "K" : Colors.lightGreen, "L" : Colors.lightGreen[800],
+      "M" : Colors.green, "N" : Colors.green[900],
+      "Ñ" : Colors.cyan, "O" : Colors.cyan[800],
+      "P" : Colors.lightBlue, "Q" : Colors.lightBlue[900],
+      "R" : Colors.grey, "S" : Colors.grey[900],
+      "T" : Colors.indigo, "U" : Colors.indigo[800],
+      "V" : Colors.purple, "W" : Colors.purple[800],
+      "X" : Colors.brown, "Y" : Colors.brown[800],
+      "Z" : Colors.blueGrey};
+    return colores_letra_inicial[letraInicial];
+  }
 }
 
 const KClientes = const <Cliente> [
-  const Cliente(nombre: "Jhoan ", correo: "jhoan@correo.com"),
-  const Cliente(nombre: "Tepox ", correo: "enrique@correo.com"),
-  const Cliente(nombre: "Ricardo ", correo: "ricardo.mendez@correo.com"),
-  const Cliente(nombre: "Nestor ", correo: "nestor.miranda@correo.com"),
-  const Cliente(nombre: "Guadalupe ", correo: "guadalupe.galvan@correo.com"),
   const Cliente(nombre: "Armando ", correo: "armando.martinez@correo.com"),
-  const Cliente(nombre: "Jose ", correo: "jose.antonio@correo.com"),
-  const Cliente(nombre: "Jose ", correo: "jose.gachuzo@correo.com"),
+  const Cliente(nombre: "Bruno ", correo: "Bruno@correo.com"),
+  const Cliente(nombre: "Carlos ", correo: "Carlos@correo.com"),
+  const Cliente(nombre: "Dario ", correo: "Dario@correo.com"),
+  const Cliente(nombre: "Ernesto ", correo: "Ernesto@correo.com"),
+  const Cliente(nombre: "Fatima ", correo: "Fatima@correo.com"),
+  const Cliente(nombre: "Guadalupe ", correo: "guadalupe.galvan@correo.com"),
+  const Cliente(nombre: "Gabriel ", correo: "Gabriel@correo.com"),
+  const Cliente(nombre: "Hortencia ", correo: "Hortencia@correo.com"),
+  const Cliente(nombre: "Irais ", correo: "Irais@correo.com"),
+  const Cliente(nombre: "Jhoan ", correo: "jhoan@correo.com"),
+  const Cliente(nombre: "Karen ", correo: "Karen@correo.com"),
+  const Cliente(nombre: "Lulu ", correo: "Lulu@correo.com"),
+  const Cliente(nombre: "Mario ", correo: "Mario@correo.com"),
+  const Cliente(nombre: "Nancy ", correo: "Nancy@correo.com"),
+  const Cliente(nombre: "Nestor ", correo: "nestor.miranda@correo.com"),
+  const Cliente(nombre: "Ñu ", correo: "Ñu@correo.com"),
+  const Cliente(nombre: "Olga ", correo: "Olga@correo.com"),
+  const Cliente(nombre: "Pedro ", correo: "Pedro@correo.com"),
+  const Cliente(nombre: "Quintana ", correo: "Quintana@correo.com"),
+  const Cliente(nombre: "Ricardo ", correo: "ricardo.mendez@correo.com"),
+  const Cliente(nombre: "Sonia ", correo: "Sonia@correo.com"),
+  const Cliente(nombre: "Tepox ", correo: "enrique@correo.com"),
+  const Cliente(nombre: "Uriel ", correo: "Uriel@correo.com"),
+  const Cliente(nombre: "Vanesa ", correo: "Vanesa@correo.com"),
+  const Cliente(nombre: "Wolf ", correo: "Wolf@correo.com"),
+  const Cliente(nombre: "Xe ", correo: "Xe@correo.com"),
+  const Cliente(nombre: "Yolanda ", correo: "Yolanda@correo.com"),
+  const Cliente(nombre: "Zaria ", correo: "Zaria@correo.com"),
+
 ];
